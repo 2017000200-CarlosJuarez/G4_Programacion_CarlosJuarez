16f00bbe6b0de5dbc2d52c4c7303c6c8cbf033a3
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-09 22:37:16 -0600

    Add files via upload

---
+++ Actividad 2 Leccion 11, Inciso 2 v.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int i = 1;
+
+        do
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+
+            i=i+1;
+
+        } while (i <= 100);
+    }
+}
