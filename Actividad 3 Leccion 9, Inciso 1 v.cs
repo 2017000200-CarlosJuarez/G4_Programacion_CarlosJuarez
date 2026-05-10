0c1981eb70de9ed27713cb9b27c2f2f1b59fc6b8
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-09 20:47:46 -0600

    Add files via upload

---
+++ Actividad 3 Leccion 9, Inciso 1 v.cs
@@ -0,0 +1,26 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n, i = 0;
+
+        Console.WriteLine("Ingrese un numero");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Numeros impares:");
+
+        while (i <= n)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+
+            i=i+1;
+        }
+
+        Console.WriteLine("Numero ingresado: " + n);
+    }
+}
