1e11b5d62f01ff78b5d6b4e4aeed1c340005a61c
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-09 22:10:19 -0600

    Add files via upload

---
+++ Actividad 1 Leccion 11  v.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a=0, l=200;
+
+        do
+        {
+            Console.WriteLine("Cajones disponibles: " + l);
+            a=a+1;
+            l=200 - a;
+        } while (l > 0);
+
+        Console.WriteLine("Ya no hay cajones libres");
+    }
+}
