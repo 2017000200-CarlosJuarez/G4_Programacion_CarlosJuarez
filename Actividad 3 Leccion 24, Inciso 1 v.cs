043ee19701a70d5c707dd5016e29668cb146fafd
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-10 01:22:41 -0600

    Add files via upload

---
+++ Actividad 3 Leccion 24, Inciso 1 v.cs
@@ -0,0 +1,26 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int d, s = 0;
+
+        for (int n = 1; n <= 50; n=n+1)
+        {
+            d = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    d=d+1;
+                }
+            }
+            if (d == 2)
+            {
+                s = s + n;
+            }
+        }
+
+        Console.WriteLine("La suma de los numeros primos es: " + s);
+    }
+}
