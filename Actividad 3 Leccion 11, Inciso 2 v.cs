fb4ed20d6b3b1e17ebdce307e3a018f573ed4539
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-09 23:14:41 -0600

    Add files via upload

---
+++ Actividad 3 Leccion 11, Inciso 2 v.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n, s=0;
+
+        do
+        {
+            Console.WriteLine("Ingrese un numero");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n != 9)
+            {
+                s= s + n;
+            }
+        } while (n != 9);
+
+        Console.WriteLine("La suma es: " + s);
+    }
+}
