a2b1d6f5f105ff3b97505340a36154aeb17c471f
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-10 01:03:34 -0600

    Add files via upload

---
+++ Actividad 3 Leccion 15, Inciso 2 v.cs
@@ -0,0 +1,33 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int c=0, s=0;
+
+        for (int n = 300; n >= 1; n=n-1)
+        {
+            Console.WriteLine(n);
+            int d = 0;
+
+            for (int i = 1; i <= n; i=i+1)
+            {
+                if (n % i == 0)
+                {
+                    d=d+1;
+                }
+            }
+            if (d == 2)
+            {
+                c=c+1;
+            }
+            if (n % 2 == 0)
+            {
+                s = s + n;
+            }
+        }
+
+        Console.WriteLine("Cantidad de numeros primos: " + c);
+        Console.WriteLine("Suma de numeros pares: " + s);
+    }
+}
