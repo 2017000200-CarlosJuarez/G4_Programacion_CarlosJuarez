3ad606b255d95ccff5903483fb35d750d5d168ef
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-09 22:45:51 -0600

    Add files via upload

---
+++ Actividad 2 Leccion 11, Inciso 3 v.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n1, n2;
+
+        Console.WriteLine("Ingrese el primer numero");
+        n1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo numero");
+        n2 = Convert.ToInt32(Console.ReadLine());
+
+        do
+        {
+            if (n1 % 4 == 0)
+            {
+                Console.WriteLine(n1);
+            }
+
+            n1=n1+1;
+
+        } while (n1 <= n2);
+    }
+}
