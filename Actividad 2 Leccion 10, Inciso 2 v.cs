dfe22fc99ebda936c7370df35d969219155a23c9
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-09 21:45:24 -0600

    Add files via upload

---
+++ Actividad 2 Leccion 10, Inciso 2 v.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n;
+
+        Console.WriteLine("Ingrese un numero");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 2; i <= n; i=i+1)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
