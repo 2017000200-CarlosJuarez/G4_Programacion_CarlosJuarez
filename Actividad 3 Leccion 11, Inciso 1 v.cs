9398b866c7e1fdda23b751e2ff323101db416cb4
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-09 22:59:29 -0600

    Add files via upload

---
+++ Actividad 3 Leccion 11, Inciso 1 v.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n1, n2, s = 0;
+
+        Console.WriteLine("Ingrese el primer numero");
+        n1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese el segundo numero");
+        n2 = Convert.ToInt32(Console.ReadLine());
+
+        n1=n1+1;
+        do
+        {
+            if (n1 < n2)
+            {
+                s = s + n1;
+            }
+            n1=n1+1;
+        } while (n1 < n2);
+
+        Console.WriteLine("La suma es: " + s);
+    }
+}
