63a124a0e2c4dec601e7faef2290d72eb8c7f1dd
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-09 22:00:22 -0600

    Add files via upload

---
+++ Actividad 3 Leccion 10 v.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int s = 0;
+
+        for (int n = 1; n <= 22; n=n+1)
+        {
+            int c = 0;
+            for (int i = 1; i <= n; i=i+1)
+            {
+                if (n % i == 0)
+                {
+                    c=c+1;
+                }
+            }
+
+            if (c == 2)
+            {
+                s = s + n;
+            }
+        }
+
+        Console.WriteLine("La suma de los numeros primos es: " + s);
+    }
+}
