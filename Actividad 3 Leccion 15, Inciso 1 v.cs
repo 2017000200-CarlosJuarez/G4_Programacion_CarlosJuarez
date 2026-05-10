df83e0c173a90a8f1b3e6c06ce092bc7badae01e
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-10 00:52:31 -0600

    Add files via upload

---
+++ Actividad 3 Leccion 15, Inciso 1 v.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int m, s=0, c=0;
+
+        for (int i = 1; i <= 20; i++)
+        {
+            m = i*7;
+            s = s+m;
+            if (m%2 == 0)
+            {
+                c=c+1;
+            }
+        }
+
+        Console.WriteLine("Suma de los multiplos: " + s);
+        Console.WriteLine("Cantidad de multiplos pares: " + c);
+    }
+}
