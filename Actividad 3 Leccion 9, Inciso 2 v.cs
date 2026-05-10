166708e107dd81ed69e25667e6de1a4984480840
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-09 21:07:25 -0600

    Add files via upload

---
+++ Actividad 3 Leccion 9, Inciso 2 v.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n, i=1, c=0;
+
+        Console.WriteLine("Ingrese un numero");
+        n= Convert.ToInt32(Console.ReadLine());
+
+        while (i <= n)
+        {
+            if (n % i == 0)
+            {
+                c=c+1;
+            }
+
+            i=i+1;
+        }
+
+        if (c == 2)
+        {
+            Console.WriteLine("Es primo");
+        }
+        else
+        {
+            Console.WriteLine("No es primo");
+        } 
+    }
+}
