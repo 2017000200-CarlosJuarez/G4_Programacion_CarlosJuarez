91baba85762e34ad29754990cf2e9298656fcf98
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-10 01:28:40 -0600

    Add files via upload

---
+++ Actividad 3 Leccion 24, Inciso 2 v.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n, s = 0;
+
+        Console.WriteLine("Ingrese un numero");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = n; i >= 1; i=i-1)
+        {
+            Console.WriteLine(i);
+            s = s + i;
+        }
+
+        Console.WriteLine("La suma es: " + s);
+    }
+}
