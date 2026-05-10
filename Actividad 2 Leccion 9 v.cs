f7be2412a0c28eaed4bcd8d38deab6e2ecd8f0cb
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-09 20:31:33 -0600

    Add files via upload

---
+++ Actividad 2 Leccion 9 v.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Niveles");
+        int m = 0, ma = 0, mc;
+        
+        while (m < 350 ) {
+            Console.WriteLine("Ingrese su cantidad de monedas");
+            mc = Convert.ToInt32(Console.ReadLine());
+            m = m + mc;
+            Console.WriteLine("Su cantidad de monedas es " + m );
+        }
+        if ( m >= 350 ) {
+            ma = ma + 1;
+            Console.WriteLine("Usted a subido de nivel");
+            Console.WriteLine("Su nivel es 5");
+        }
+        //< >
+    }
+}
