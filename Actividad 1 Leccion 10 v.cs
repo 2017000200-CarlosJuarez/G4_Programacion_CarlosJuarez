7cca448e9cf973be0370a6989f566e7f97ee77b4
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-09 21:29:05 -0600

    Add files via upload

---
+++ Actividad 1 Leccion 10 v.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const int ts = 8;
+
+        Console.WriteLine("Bienvenido a la Barberia PRESI");
+
+        for (int i = 1; i <= ts; i++)
+        {
+            Console.WriteLine("Sellos actuales: {0} de {1}", i, ts);
+            Console.WriteLine("Presione cualquier tecla para registrar esta visita");
+            Console.ReadKey();
+            Console.WriteLine(">>>Sello #{0}: Registrado correctamente", i);
+        }
+
+        Console.WriteLine("\n------------------------");
+        Console.WriteLine("cuponera completada");
+        Console.WriteLine("David se a llevado un corte gratis");
+        Console.WriteLine("------------------------");
+    }
+}
