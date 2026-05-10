2521e3bc7a2a33641c92f053cedef09cb06b33ee
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-09 22:23:41 -0600

    Add files via upload

---
+++ Actividad 2 Leccion 11, Inciso 1 v.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n, f=1, i=1;
+
+        Console.WriteLine("Ingrese un numero");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        do
+        {
+            f = f * i;
+            i=i+1;
+
+        } while (i <= n);
+
+        Console.WriteLine("El factorial es: " + f);
+    }
+}
