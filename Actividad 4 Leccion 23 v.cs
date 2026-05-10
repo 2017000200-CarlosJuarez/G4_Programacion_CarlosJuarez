50ead3505d45361d09e29dabdc691ac7115c8246
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-10 01:10:30 -0600

    Add files via upload

---
+++ Actividad 4 Leccion 23 v.cs
@@ -0,0 +1,19 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n, i = 1, v;
+
+        Console.WriteLine("Ingrese un numero");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        while (i <= 10)
+        {
+            v = n * i;
+            Console.WriteLine(n + " x " + i + " = " + v);
+
+            i=i+1;
+        }
+    }
+}
