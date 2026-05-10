cb858e45a2bbba73917ef4fb5cd7fc4102dafe2f
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-09 21:37:38 -0600

    Add files via upload

---
+++ Actividad 2 Leccion 10, Inciso 1 v.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int m;
+
+        Console.WriteLine("Primeros 20 multiplos de 4");
+
+        for (int i = 1; i <= 20; i=i+1)
+        {
+            m = i * 4;
+
+            Console.WriteLine(m);
+        }
+    }
+}
