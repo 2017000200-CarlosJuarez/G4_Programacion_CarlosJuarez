e60e25867baabb5014b86b0d78300575769a0bea
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-05-10 00:42:58 -0600

    Add files via upload

---
+++ Actividad 2 Leccion 15, Inciso 2 v.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int b1, b2, b3, tb, td;
+
+        Console.WriteLine("Cantidad de billetes de 20");
+        b1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Cantidad de billetes de 50");
+        b2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Cantidad de billetes de 100");
+        b3 = Convert.ToInt32(Console.ReadLine());
+
+        tb = b1 + b2 + b3;
+        td = b3 * 100;
+
+        Console.WriteLine("Total de billetes: " + tb);
+        Console.WriteLine("Total de Billetes de 100: " + b3);
+        Console.WriteLine("Total de dinero de los billetes de 100: Q" + td);
+    }
+}
